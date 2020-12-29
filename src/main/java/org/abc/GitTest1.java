08733c7179519a090a7a243379f9407e5faf43ec
hemumaQA <hemuma85@gmail.com>
2020-12-28 23:50:45 -0800

    commiting file to git

---
+++ src/main/java/org/abc/GitTest1.java
@@ -0,0 +1,12 @@
+package org.abc;
+
+public class GitTest1 {
+    public static void main(String[] args) {
+        String hungry ="Yes";
+        if (hungry == "yes")
+            System.out.println("eat something.......");
+        else {
+            System.out.println("Do work.....");
+        }
+    }
+}
